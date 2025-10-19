555a5217963e92caa7863268b9d42f5a6e3e68a3
Catherine <catherinewhite1013@gmail.com>
2025-10-19 19:09:55 +0800

    CanSparkMax --> SparkMax

--- robot.py
+++ robot.py
@@ -8,15 +8,15 @@ class MyRobot(wpilib.TimedRobot):
             This function is called upon program startup and
             should be used for any initialization code.
             """
-            self.leftFront = rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushed) #這怎麼調(?
-            self.leftRear = rev.CANSparkMax(4, rev.CANSparkMax.MotorType.kBrushed) #這怎麼調(?
-            self.rightFront = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushed)#這怎麼調(?
-            self.rightRear = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushed)#這怎麼調(?
+            self.leftFront = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushed) 
+            self.leftRear = rev.SparkMax(4, rev.SparkMax.MotorType.kBrushed) 
+            self.rightFront = rev.SparkMax(2, rev.SparkMax.MotorType.kBrushed)
+            self.rightRear = rev.SparkMax(3, rev.SparkMax.MotorType.kBrushed)
             self.leftRear.follow(self.leftFront)
             self.rightRear.follow(self.rightFront)
             self.robotDrive = wpilib.drive.DifferentialDrive(self.leftFront, self.rightFront)
             self.controller = wpilib.XboxController(0)
-            self.rightDrive.setInverted(True)#這個要調左右邊(看馬達方向)
+            self.rightFront.setInverted(True)
 
     def teleopInit(self):
         """This function is called once each time the robot enters teleoperated mode."""
@@ -24,5 +24,7 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         """This function is called periodically during teleoperated mode."""
         self.robotDrive.arcadeDrive(-self.controller.getLeftY(), -self.controller.getRightX())
+
+
 
     
